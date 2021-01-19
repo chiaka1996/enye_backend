51a9bf53fcf60043bec46f62808c0e24c4d6dac9
newton <chiakajunior@yahoo.com>
2021-01-19 17:35:19 +0100

    worked on the api

--- routes.js
+++ routes.js
@@ -9,40 +9,25 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-let exchangeRate;
-
-const getExchangeRate = async (base) => {
-    //console.log(base)
-    try{
-    exchangeRate = await axios.get(`https://api.exchangeratesapi.io/latest?base=${base}`);
-    return exchangeRate
-    }
-    catch(err){
-        return err
-    }
-}
-
-
 app.get('/api/rates', (req, res) => {
-        
-    let base;
-    let currency;
-    let splittedCurrency;
-    base = req.query.base;
-    currency = req.query.currency;
-    splittedCurrency = currency.split(',');
 
-     getExchangeRate(base)
-    console.log(exchangeRate)
-
-    if(base === "" || currency==="") {
-        res.status(404).json({
-           error: "the url is incorrect"
-       })
-   }
-
-   else {
+    let base = req.query.base;
+    let currency = req.query.currency;
+    let splittedCurrency = currency.split(',');
     
+    axios.get(`https://api.exchangeratesapi.io/latest?base=${base}`)
+    .then(
+        (respond) => {
+
+           let exchangeRate = respond;
+
+           if(base === "" || currency==="") {
+            res.status(404).json({
+               error: "the url is incorrect"
+           })
+       }
+
+       else {
         let exchange = exchangeRate.data.rates
         let rateObject = {}
         for (const rate in exchange) {
@@ -64,6 +49,8 @@ app.get('/api/rates', (req, res) => {
         }
    })
 }
+        
+        })
     
     });
 
